a232555ac778b75036b1129e619e6f06a58f0cee
Yves Lohier <yves.lohier@pole-emploi.fr>
2026-07-28 11:47:34 +0200

    Mise à jour du frontend

--- src/app/menu/affichage-souv/affichage-souv2/affichage-souv2.service.ts
+++ src/app/menu/affichage-souv/affichage-souv2/affichage-souv2.service.ts
@@ -12,9 +12,9 @@ export class AffichageSouv2Service {
 
   getSouv(idSouv: any): Observable<any[]> {
 
-      const apiUrl = 'https://projet-sf-backend.vercel.app/menu/affichage-souv/affichage-souv2/${idSouv}';
+      // const apiUrl = 'https://projet-sf-backend.vercel.app/menu/affichage-souv/affichage-souv2/${idSouv}';
 
-      // const apiUrl = `http://localhost:3000/menu/affichage-souv/affichage-souv2/${idSouv}`;
+      const apiUrl = `http://localhost:3000/menu/affichage-souv/affichage-souv2/${idSouv}`;
 
       // const apiUrl = `http://localhost:3000/menu/affichage-souv2/${idSouv}`;
 
